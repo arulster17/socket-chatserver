90f6c1cfabbde7a991e3307efbe04ece80674ac1
Arul Mathur <arulster17@gmail.com>
2025-07-02 22:02:13 -0700

    semaphore limiting number of threads

--- server.c
+++ server.c
@@ -7,13 +7,33 @@
 #include <sys/socket.h>
 #include <unistd.h>
 #include <pthread.h>
+#include <signal.h>
+#include <dispatch/dispatch.h>
 
 #define PORT 8080
-#define MAX_CLIENTS 10
+#define MAX_CLIENTS 2
 
+int server_fd;
 int client_list[MAX_CLIENTS];
 int available_client[MAX_CLIENTS];
-pthread_mutex_t client_list_lock = PTHREAD_MUTEX_INITIALIZER;
+dispatch_semaphore_t client_slots;
+
+struct threadinfo {
+    int fd;
+    int id;
+};
+
+void sigint_handler(int arg) {
+    printf("\nShutting down the server\n");
+    close(server_fd);
+    for (int i = 0; i < MAX_CLIENTS; i++) {
+        if(!available_client[i]) {
+            // Goodbye clients
+            close(client_list[i]);
+        }
+    }
+    exit(0);
+}
 
 void *handle_client(void *arg) {
     // This will be called whenever a new client connects
@@ -31,33 +51,58 @@ void *handle_client(void *arg) {
         send(client_fd, buffer, strlen(buffer), 0);
     }
     // Connection closed
-    printf("Client disconnected");
+    printf("Client disconnected\n");
+    dispatch_semaphore_signal(client_slots);
+    close(client_fd);
     return NULL;
 
 }
 
 //int main(int argc, char const* argv[]) {
 int main() {
+
+    signal(SIGINT, sigint_handler);
+
+    printf("hello\n");
     // Set up server socket
-    int server_fd = socket(AF_INET, SOCK_STREAM, 0);
+    server_fd = socket(AF_INET, SOCK_STREAM, 0);
     struct sockaddr_in addr;
     addr.sin_family = AF_INET;
     addr.sin_addr.s_addr = INADDR_ANY;
     addr.sin_port = htons(PORT);
+    
+    int opt = 1;
+    setsockopt(server_fd, SOL_SOCKET, SO_REUSEADDR, &opt, sizeof(opt));
+
     bind(server_fd, (const struct sockaddr *) &addr, sizeof(addr));
 
     // Listen for connections
     listen(server_fd, 5);
+   
+
+    client_slots = dispatch_semaphore_create(MAX_CLIENTS);
 
     // Accept client connection
     struct sockaddr_in client_addr;
     socklen_t client_addrlen = sizeof(client_addr);
-
+    int cnt = 1;
     while(1) {
+        // Wait for an open slot
+        printf("%d waiting for open slot\n", cnt);
+        dispatch_semaphore_wait(client_slots, DISPATCH_TIME_FOREVER);
+        printf("%d got open slot\n", cnt);
+
         int client_fd = accept(server_fd, (struct sockaddr*) &client_addr, &client_addrlen);
         pthread_t handle_client_thread;
-        pthread_create(&handle_client_thread, NULL, handle_client, &client_fd);
+
+        int *fd_ptr = malloc(sizeof(int));
+        *fd_ptr = client_fd;
+        pthread_create(&handle_client_thread, NULL, handle_client, fd_ptr);
+
+        printf("%d connected to server\n", cnt);
+        cnt++;
     }
+
 
 /*
 
